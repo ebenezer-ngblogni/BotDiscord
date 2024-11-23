7e6404aa774459b0c44abf9cabe0711f25588312
ebenezer-ngblogni <ebengamer6@gmail.com>
2024-11-23 01:02:00 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.command()
+async def bonjour(ctx):
+    await ctx.send(f"Bonjour {ctx.author} !")
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send(f"Good night {ctx.author} !")
+
+token = os.getenv("DISCORD_BOT_TOKEN")
+
+bot.run(token)
